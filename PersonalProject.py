d299978d39ef9c4e9894e3b8adcb1071956476b7
djMahad <50090863+djMahad@users.noreply.github.com>
2019-06-18 09:27:44 +1000

    Update PersonalProject.py

--- PersonalProject.py
+++ PersonalProject.py
@@ -1,28 +1,52 @@
-Arthritis = "\n\nYou most likely have Arthritis. This is a medical condition that damages the body's joints, causing discomfort and pain. It can range from mild to severe and can affect people of all ages. Arthritis most often develops in adults who are in their late 40’s or older and it is also more common in women and other people with a family history of the condition. It affects the joints of the body making movement more difficult thus causing pain and stiffness. It also causes swelling and the formation of bony spurs. Bones also start to rub with other bones altering the shape of the joint thus making it easier for the bone to pop out. The most commonly affected joints are hands, spine, knees and hips. Some symptoms of this disease include; join pain, tenderness, inflammation, restricted movement and weakness of bones. Although there is no cure for arthritis there are many treatment to slow it down. For mild to medium arthritis: Painkillers, physiotherapy and regular exercise can help reduce/slow down the effects, and for severe conditions, surgery such as: Arthroplasty, arthrodesis and osteotomy is recommended."
-Asthma = "\n"
-Commoncold = "\n"
-Cough = "\n"
-Diarrhoea = "\n"
-FLu = "\n"
-Foodpoisoning = "\n"
-Hayfever = "\n"
-Whoppingcough = "\n"
-B12defficency = "\n"
-Irondefficency = "\n"
-Foodallergies = "\n"
+Arthritis = "\n\n You most likely have ---- mild Arthritis. Arthritis is a medical condition that damages the body's joints, causing discomfort and pain. It can range from mild to severe and can affect people of all ages. Arthritis most often develops in adults who are in their late 40’s or older and it is also more common in women and other people with a family history of the condition. It affects the joints of the body making movement more difficult thus causing pain and stiffness. It also causes swelling and the formation of bony spurs. Bones also start to rub with other bones altering the shape of the joint thus making it easier for the bone to pop out. The most commonly affected joints are hands, spine, knees and hips. Some symptoms of this disease include; join pain, tenderness, inflammation, restricted movement and weakness of bones. Although there is no cure for arthritis there are many treatment to slow it down. For mild to medium arthritis: Painkillers, physiotherapy and regular exercise can help reduce/slow down the effects, and for severe conditions, surgery such as: Arthroplasty, arthrodesis and osteotomy is recommended."
+Asthma = "\n\n You most likely have ---- Asthma. Asthma is a common long-term condition that can cause coughing, wheezing, chest tightness and breathlessness. Occasionally, asthma symptoms can get gradually or suddenly worse. This is known as an asthma attack. Severe attacks may require hospital treatment and can be life threatening, although this is unusual. Common asthma triggers include; house dust mites, animal fur, pollen, cigarette smoke, exercise, viral infections. Asthma can develop at any age, including in young children and elderly people. While there is no cure for asthma, there are some treatments that can help control the condition. For most people, daily use of medications prescribed by your doctor is helpful, usually taken using an inhaler. Also identifying and avoiding possible triggers is also helpful."
+Commoncold = "\n\n You most likely have ---- a cold. A cold is a mild viral infection of the nose, throat, sinuses and upper airways. It's very common and usually clears up on its own within a week or two. The main symptoms of a cold include; a sore throat, a blocked or runny nose, sneezing, coughing. More severe symptoms, include; a high temperature (fever), headache and aching muscles. There's no cure for a cold, but you can look after yourself at home by; resting, drinking plenty of fluids and eating healthily, taking over-the-counter painkillers, such as paracetamol or ibuprofen to reduce any fever or discomfort, using decongestant sprays or tablets to relieve a blocked nose and trying remedies such as gargling salt water."
+Diarrhoea = "\n\n You most likely have ---- Diarrhoea. Diarrhoea is passing looser or more frequent stools than is normal for you. Some causes of diarrhoea include; a virus – such as norovirus or rotavirus in your body, bacteria – such as campylobacter and E. coli in your body (which are often picked up from contaminated food), a parasite in your body – which is spread in contaminated water. Diarrhoea can also be the result of anxiety, a food allergy, medication, or a long-term condition, such as irritable bowel syndrome (IBS). Most cases of diarrhoea clear up after a few days without treatment, and you may not need to see your GP. However, diarrhoea can lead to dehydration, so you should drink plenty of fluids (small, frequent sips of water)."
+Flu = "\n\n You most likely have ---- a Flu. The Flu is a common infectious viral illness spread by coughs and sneezes. It can be very unpleasant, but you'll usually begin to feel better within about a week. You can catch flu all year round, but it's especially common in winter, which is why it's also known as seasonal flu. Some of the main symptoms of flu include; a high temperature (fever) of 38C (100.4F) or above, tiredness and weakness, headaches, general aches and pains, a dry, chesty cough. Cold-like symptoms, such as a blocked or runny nose, sneezing, and a sore throat, can also be caused by the flu, but they tend to be less severe than the other symptoms you have. The best remedy is to rest at home, keep warm and drink plenty of water to avoid dehydration. You can take paracetamol or ibuprofen to lower a high temperature and relieve aches if necessary."
+Foodpoisoning = "\n\n You most likely have ---- Food Poisoning. Food Poisoning is an illness caused by eating contaminated food. It's not usually serious and most people get better within a few days without treatment. The symptoms of food poisoning usually begin within one to two days of eating contaminated food, although they may start at any point between a few hours and several weeks later. The main symptoms include: feeling sick (nausea), Vomiting, diarrhoea, stomach cramps and abdominal pain, a lack of energy and weakness, loss of appetite, a high temperature (fever), aching muscles, chills. The best way to aid the recovery with food poisoning is to take a lot of fluids (even little sips) and try to eat as much as possible (bland foods help the best)."
+Hayfever = "\n\n You most likely have ---- a Hay Fever. Hay Fever is a common allergic condition that affects up to one in five people at some point in their life. You'll experience hay fever symptoms if you have an allergic reaction to pollen (Grass, Tree, Weed Pollen). Some symptoms of hay fever include: sneezing, runny nose and itchy eyes. There's currently no cure for hay fever, but most people are able to relieve symptoms with treatment, at least to a certain extent. The most effective way to control hay fever would be to avoid exposure to pollen. Some other treatment options for hay fever include antihistamines, which can help to prevent an allergic reaction from occurring and corticosteroids (steroids), which help to reduce inflammation and swelling."
+Whoopingcough = "\n\n You most likely have ---- a Whooping Cough. Whooping cough is a highly contagious bacterial infection of the lungs and airways. The first symptoms of whooping cough are similar to those of a cold, such as a runny nose, red and watery eyes, a sore throat, and a slightly raised temperature. Treatment for whooping cough depends on your age and how long you've had the infection. While you're recovering at home, it can be helpful to get plenty of rest, drink lots of fluids, and take painkillers such as paracetamol or ibuprofen for a fever."
+Irondefficency = "\n\n You most likely have ---- Iron defficiency. Iron defficiency is a condition where a lack of iron in the body leads to a reduction in the number of red blood cells. The most common symptoms include: tiredness and lack of energy (lethargy), shortness of breath, noticeable heartbeats (heart palpitations), a pale complexion. One medical treatment for iron deficiency involves taking iron supplements to boost the low levels of iron in your body. Also Increasing the amount of iron in your diet is also recommended. Good sources of iron include: dark-green leafy vegetables, iron-fortified cereals or bread, brown rice, pulses and beans, nuts and seeds, meat, fish and tofu, eggs, dried fruit (such as dried apricots, prunes and raisins)."
+B12defficency = "\n\n You most likely have ---- Vitamin B12 defficiency. This defficency occurs when a lack of vitamin B12 causes the body to produce abnormally large red blood cells that can't function properly. A deficiency in either of these vitamins can cause a wide range of problems, including: extreme tiredness, a lack of energy, pins and needles, a sore and red tongue, muscle weakness, disturbed vision. It also might cause some psychological problems such as; confusion, problems with memory, understanding and judgement. Most cases of vitamin B12 deficiency can be easily treated with injections or tablets to replace the missing vitamins by your GP. Also, improving your diet can help treat the condition and prevent it recurring. Vitamin B12 is found in meat, fish, eggs, dairy products, yeast extract (such as Vegemite) and specially fortified foods. The best sources of B12 include green vegetables such as broccoli, brussels sprouts and peas."
 
 print("\n                                                      WELCOME TO THE DISEASE DIAGNOSTICS PROGRAM")
 begin = input("\n                                      Would you like to begin?: ")
 if begin == "yes" or begin == "Yes" or begin == "YES":
-    print("\n\n\n                                   Choose one of the options below for all the following question")
+    print("\n\n\n                                   Choose one of the options below for all of the following question")
     print("\n\n                                       YES")
     print("\n                                       NO")
-    Q1 = input("\n\n\n                              Do you feel pain in your joints/weakness in your bones?: ")
+    Q1 = input("\n\n\n                              Do you have a runny/blocked nose?: ")
     if Q1 == "yes" or Q1 == "Yes" or Q1 == "YES":
-        print(Arthritis)
+        Q2 = input("\n\n\n                          Do you sometimes experience high tempertures/Fever?: ")
+        if Q2 == "yes" or Q2 == "Yes" or Q2 == "YES":
+            Q4 = input("\n\n\n                     Do you often experience sneezing or coughing?: ")
+            if Q4 == "yes" or Q4 == "Yes" or Q4 == "YES":
+                Q5 = input("\n\n\n                    Do you have a sore throat?: ")
+                if Q5 == "yes" or Q5 == "Yes" or Q5 == "YES":
+                    print(Flu)
+                else:
+                    print(Flu)
+            else:
+                print(Commoncold)
+        else:
+            print(Whoopingcough)
     else:
-        Q2 = input("\n\n\n                              Do you have headaches often")
-        print(                             Q2)
+        Q3 = input("\n\n\n                Do you experience lack of energy from time to time?: ")
+        if Q3 == "yes" or Q3 == "Yes" or Q3 == "YES":
+            Q6 = input("\n\n\n                   Do you ever experience pins and needles?: ") 
+            if Q6 == "yes" or Q6 == "Yes" or Q6 == "YES":
+                print(B12defficency)
+            else:
+                print(Irondefficency)
+        else:
+            Q8 = input("\n\n\n                Do you often experience vomiting/nausea?: ")
+            if Q8 == "yes" or Q8 == "Yes" or Q8 == "YES":
+                print (Foodpoisoning)
+            else:
+                print(Diarrhoea)
+        
 else:
     print("\n                                         THANKS FOR TRYING OUT THE DISEASE DIAGNOSTIC PROGRAM!")
+ 
+
  
